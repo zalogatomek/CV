68b41967cbd8ca55b530d7e80fd98dcf7ebb1131
Tomasz Załoga <zalogatomek@gmail.com>
2020-03-21 12:38:29 +0100

    Create empty xcode project

---
+++ CV App/CV App/Scenes/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Tomasz Załoga on 21/03/2020.
+//  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
