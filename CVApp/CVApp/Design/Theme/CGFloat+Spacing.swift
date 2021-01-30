638c9772cae41e20ddff3d2d1bb6cabbba987f93
Tomasz Załoga <zalogatomek@gmail.com>
2021-01-30 17:04:03 +0100

    Update margins/spacing for regular size screens

--- CVApp/CVApp/Design/Theme/CGFloat+Spacing.swift
+++ CVApp/CVApp/Design/Theme/CGFloat+Spacing.swift
@@ -6,9 +6,14 @@ import SwiftUI
 
 extension CGFloat {
     
+    private static var spacingMultipier: CGFloat {
+        let horizontalSizeClass = UIApplication.shared.windows.first?.rootViewController?.traitCollection.horizontalSizeClass
+        return horizontalSizeClass == .regular ? 1.5 : 1.0
+    }
+    
     // MARK: - Spacing values
     
-    static var small: CGFloat { 8.0 }
-    static var standard: CGFloat { 16.0 }
-    static var wide: CGFloat { 32.0 }
+    static var small: CGFloat { 8.0 * spacingMultipier }
+    static var standard: CGFloat { 16.0 * spacingMultipier }
+    static var wide: CGFloat { 32.0 * spacingMultipier }
 }
