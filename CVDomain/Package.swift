273041ae36505df5f917cbf5a5ca3dcb37b7da95
Tomasz Załoga <zalogatomek@gmail.com>
2021-02-18 21:20:02 +0100

    Add RxSwift dependency for CVDomain

--- CVDomain/Package.swift
+++ CVDomain/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,16 +6,21 @@ import PackageDescription
 let package = Package(
     name: "CVDomain",
     products: [
-        .library(
-            name: "CVDomain",
-            targets: ["CVDomain"]),
+        .library(name: "CVDomain", targets: ["CVDomain"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
     ],
     targets: [
         .target(
             name: "CVDomain",
-            dependencies: []),
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+            ]
+        ),
         .testTarget(
             name: "CVDomainTests",
-            dependencies: ["CVDomain"]),
+            dependencies: ["CVDomain"]
+        )
     ]
 )
